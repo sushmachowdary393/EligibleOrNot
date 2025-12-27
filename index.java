0f7788368d615f6c2a7ca24347d3eebce710e151
Yalamati Sushma Chowdary <sushmachowdary393@gmail.com>
2025-12-27 16:51:28 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class index {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter total classes: ");
+        int total = sc.nextInt();
+        System.out.print("Enter attended classes: ");
+        int attended = sc.nextInt();
+
+        double percent = (attended * 100.0) / total;
+
+        System.out.println("Attendance Percentage: " + percent + "%");
+
+        if (percent >= 75)
+            System.out.println("Eligible for Exam");
+        else
+            System.out.println("Not Eligible for Exam");
+    }
+}
